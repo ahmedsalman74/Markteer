3c8166b7d29a30f0b1af0ef9dacbca50755c6279
Ahmed salman <79092734+ahmedsalman74@users.noreply.github.com>
2024-03-22 01:26:10 +0200

    Add calcAverageRatingsAndQuantity method to reviewModel.js

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Product = require('./productModel');
 
 const reviewSchema = new mongoose.Schema({
     title: {
@@ -50,5 +51,37 @@ reviewSchema.pre(/^find/, function (next) {
     });
     next();
 });
+
+reviewSchema.statics.calcAverageRatingsAndQuantity = async function (
+    productId
+  ) {
+    const result = await this.aggregate([
+      // Stage 1 : get all reviews in specific product
+      {
+        $match: { product: productId },
+      },
+      // Stage 2: Grouping reviews based on productID and calc avgRatings, ratingsQuantity
+      {
+        $group: {
+          _id: 'product',
+          avgRatings: { $avg: '$ratings' },
+          ratingsQuantity: { $sum: 1 },
+        },
+      },
+    ]);
+
+      // console.log(result);
+  if (result.length > 0) {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsAverage: result[0].avgRatings,
+      ratingsQuantity: result[0].ratingsQuantity,
+    });
+  } else {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsAverage: 0,
+      ratingsQuantity: 0,
+    });
+  }
+};
 
 module.exports = mongoose.model('review', reviewSchema);
