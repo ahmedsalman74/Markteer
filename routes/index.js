2172be4f4e7db73a2499f540c80b1de4505decac
Ahmed salman <79092734+ahmedsalman74@users.noreply.github.com>
2024-03-24 03:13:56 +0200

    Refactor server.js to use mountRouts for routing

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+
+//import routs
+const categoryRoute = require('./categoryRoutes');
+const productRoute = require('./productRoutes');
+const brandRoute = require('./brandRoutes');
+const reviewRoute = require('./reviewRoutes');
+const subCategoryRoute = require('./subCategoryRoutes');
+const wishlistRoute = require('./wishlistRoutes');
+const addressRoute = require('./addressRouts');
+const couponRoute = require('./couponRoutes');
+
+const userRoute = require('./userRoutes');
+const authRoute = require('./authRoutes');
+
+
+
+const mountRouts = (app) => {
+    app.use('/api/v1/categories', categoryRoute);
+    app.use('/api/v1/products', productRoute);
+    app.use('/api/v1/brands', brandRoute);
+    app.use('/api/v1/subCategories', subCategoryRoute);
+    app.use('/api/v1/users', userRoute);
+    app.use('/api/v1/auth', authRoute);
+    app.use('/api/v1/reviews', reviewRoute);
+    app.use('/api/v1/wishlist', wishlistRoute);
+    app.use('/api/v1/address', addressRoute);
+    app.use('/api/v1/coupons', couponRoute);
+}
+
+module.exports = mountRouts;
--- server.js
+++ server.js
@@ -7,18 +7,8 @@ dotenv.config({ path: 'config.env' })
 const dbConnection = require('./config/connections');
 const AppError = require('./utils/appError');
 const globalErrors = require('./middlewares/errorMiddleware');
-//import routs
-const categoryRoute = require('./routes/categoryRoutes');
-const productRoute = require('./routes/productRoutes');
-const brandRoute = require('./routes/brandRoutes');
-const reviewRoute = require('./routes/reviewRoutes');
-const subCategoryRoute = require('./routes/subCategoryRoutes');
-const wishlistRoute = require('./routes/wishlistRoutes');
-const addressRoute = require('./routes/addressRouts');
-const couponRoute = require('./routes/couponRoutes');
-
-const userRoute = require('./routes/userRoutes');
-const authRoute = require('./routes/authRoutes');
+// import routes
+const mountRouts = require('./routes');
 
 
 // DB connection
@@ -42,16 +32,7 @@ if (NODE_ENV === 'development') {
 
 
 //router routes
-app.use('/api/v1/categories', categoryRoute);
-app.use('/api/v1/products', productRoute);
-app.use('/api/v1/brands', brandRoute);
-app.use('/api/v1/subCategories', subCategoryRoute);
-app.use('/api/v1/users', userRoute);
-app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/reviews', reviewRoute);
-app.use('/api/v1/wishlist', wishlistRoute);
-app.use('/api/v1/address', addressRoute);
-app.use('/api/v1/coupons', couponRoute);
+mountRouts(app);
 
 //error routes handler
 app.all('*', (req, res, next) => {
