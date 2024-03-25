f748ebc3079a71e6cb200d99f6a4e21e6adb24b4
Ahmed salman <79092734+ahmedsalman74@users.noreply.github.com>
2024-03-25 16:33:49 +0200

    Add cart functionality |removeSpecificCartItem| clearCart |updateCartItemQuantity

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -80,36 +80,76 @@ const addProductToCart = asyncHandler(async (req, res, next) => {
 
 const getLoggedUserCart = asyncHandler(async (req, res, next) => {
     const cart = await cartModel.findOne({ user: req.user._id });
-  
+
     if (!cart) {
-      return next(
-        new AppError(`There is no cart for this user id : ${req.user._id}`, 404)
-      );
+        return next(
+            new AppError(`There is no cart for this user id : ${req.user._id}`, 404)
+        );
     }
-  
+
     res.status(200).json({
-      status: 'success',
-      numOfCartItems: cart.cartItems.length,
-      data: cart,
+        status: 'success',
+        numOfCartItems: cart.cartItems.length,
+        data: cart,
     });
-  });
+});
+
+// @desc    Remove specific cart item
+// @route   DELETE /api/v1/cart/:itemId
+// @access  Private/User
+const removeSpecificCartItem = asyncHandler(async (req, res, next) => {
+    const cart = await cartModel.findOneAndUpdate(
+        { user: req.user._id },
+        {
+            $pull: { cartItems: { _id: req.params.itemId } },
+        },
+        { new: true }
+    );
+
+    calcTotalCartPrice(cart);
+    cart.save();
+
+    res.status(200).json({
+        status: 'success',
+        numOfCartItems: cart.cartItems.length,
+        data: cart,
+    });
+});
+
+// @desc    clear logged user cart
+// @route   DELETE /api/v1/cart
+// @access  Private/User
+const clearCart = asyncHandler(async (req, res, next) => {
+    await cartModel.findOneAndDelete({ user: req.user._id });
+    res.status(204).send();
+});
+
 // @desc    update logged user chart
 // @route   PUT /api/v1/chart
 // @access  privet /user
 
-const updateCart = asyncHandler(async (req, res, next) => {
+const updateCartItemQuantity = asyncHandler(async (req, res, next) => {
+    const { quantity } = req.body
+
     const cart = await cartModel.findOne({ user: req.user._id });
     if (!cart) {
         return next(new AppError(`There is no cart for this user id : ${req.user._id}`, 404));
     }
-    const { cartItems } = req.body;
-    cart.cartItems = cartItems;
-    // Calculate total cart price
+    const indexItem = cart.cartItems.findIndex((item) => item._id.toString() === req.params.itemId);
+    if (indexItem > -1) {
+        cart.cartItems[indexItem].quantity = quantity;
+
+    } else {
+        return next(new AppError(`There is no item with this id : ${req.params.itemId}`, 404));
+
+    }
     calcTotalCartPrice(cart);
+
     await cart.save();
+
     res.status(200).json({
         status: 'success',
-        message: 'Cart updated successfully',
+        numOfCartItems: cart.cartItems.length,
         data: cart,
     });
 });
@@ -117,5 +157,8 @@ const updateCart = asyncHandler(async (req, res, next) => {
 module.exports = {
     addProductToCart,
     getLoggedUserCart,
-    updateCart
+    removeSpecificCartItem,
+    clearCart,
+    updateCartItemQuantity
+
 };
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,20 +1,25 @@
 const router = require('express').Router();
 const {
     addProductToCart,
-    getLoggedUserCart 
+    getLoggedUserCart,
+    removeSpecificCartItem,
+    clearCart,
+    updateCartItemQuantity
 } = require('../controllers/cartController');
 
 
 const authService = require('../controllers/authController')
 
-router.use(authService.protect,authService.allowedTo('user'))
+router.use(authService.protect, authService.allowedTo('user'))
 router.route('/')
-   .get(getLoggedUserCart)
+    .get(getLoggedUserCart)
     .post(addProductToCart)
+    .delete(clearCart)
 
-router.route('/:id')
-    .get()
-    
+router.route('/:itemId')
+    .delete(removeSpecificCartItem)
+    .put(updateCartItemQuantity)
+
 
 
 module.exports = router; 
