9fa59268311ce0196c3139235d824e0e7c9a7637
Ahmed salman <79092734+ahmedsalman74@users.noreply.github.com>
2024-03-26 23:21:06 +0200

    Refactor createOrder function and add filterOrderForLoggedUser function

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,54 +5,73 @@ const Cart = require('../models/cartModel');
 const Product = require('../models/productModel');
 
 const AppError = require('../utils/appError');
+const factory = require('./handlersFactory');
 
 // @desc create new order
 // @route POST /api/v1/orders
 // @access protected/user
 
 const createOrder = asyncHandler(async (req, res, next) => {
-    //1) get cart depend on cartId
-    const cart = await Cart.findById(req.body.cartId);
+    // app settings
+    const taxPrice = 0;
+    const shippingPrice = 0;
+
+    // 1) Get cart depend on cartId
+    const cart = await Cart.findById(req.params.cartId);
     if (!cart) {
-        return next(new AppError('Cart not found', 404));
+        return next(
+            new AppError(`There is no such cart with id ${req.params.cartId}`, 404)
+        );
     }
 
-    //2) get order price dependent on cart price "check" if coupon applied
-    const cartPrice = cart.totalPriceAfterDiscount ? cart.totalPriceAfterDiscount : cart.totalCartPrice;
-    const totalOrderPrice = cartPrice + cart.taxPrice + cart.shippingPrice;
-    //3) create order with default paymentMethodType: cash
+    // 2) Get order price depend on cart price "Check if coupon apply"
+    const cartPrice = cart.totalPriceAfterDiscount
+        ? cart.totalPriceAfterDiscount
+        : cart.totalCartPrice;
+
+    const totalOrderPrice = cartPrice + taxPrice + shippingPrice;
+
+    // 3) Create order with default paymentMethodType cash
     const order = await Order.create({
         user: req.user._id,
         cartItems: cart.cartItems,
-        taxPrice: cart.taxPrice,
-        shippingAddress: cart.shippingAddress,
-        shippingPrice: cart.shippingPrice,
+        shippingAddress: req.body.shippingAddress,
         totalOrderPrice,
     });
-    //4) after creating order decreaces ordered product increment product sold
+
+    // 4) After creating order, decrement product quantity, increment product sold
     if (order) {
-        const blukOption = cart.cartItems.map((item) => ({
+        const bulkOption = cart.cartItems.map((item) => ({
             updateOne: {
                 filter: { _id: item.product },
                 update: { $inc: { quantity: -item.quantity, sold: +item.quantity } },
             },
         }));
-        await Product.bulkWrite(blukOption, {});
-        //5) clear cart depend on cartId
-        await cart.findByIdAndDelete(req.params.cartId);
+        await Product.bulkWrite(bulkOption, {});
 
+        // 5) Clear cart depend on cartId
+        await Cart.findByIdAndDelete(req.params.cartId);
     }
-    res.status(201).json({
-        status: 'success',
-        data: {
-            order,
-        },
-    });
 
+    res.status(201).json({ status: 'success', data: order });
 });
 
+const filterOrderForLoggedUser = asyncHandler(async (req, res, next) => {
+    if (req.user.role === 'user') req.filterObj = { user: req.user._id };
+    next();
+});
 
+// @desc    Get all orders
+// @route   POST /api/v1/orders
+// @access  Protected/User-Admin-Manager
+exports.findAllOrders = factory.getAll(Order);
+
+// @desc    Get all orders
+// @route   POST /api/v1/orders
+// @access  Protected/User-Admin-Manager
+exports.findSpecificOrder = factory.getOne(Order);
 
 module.exports = {
     createOrder,
+    filterOrderForLoggedUser
 };
