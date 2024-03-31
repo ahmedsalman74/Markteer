5cc44d320f39c09c92346738cd4316d809228a95
Ahmed salman <79092734+ahmedsalman74@users.noreply.github.com>
2024-03-31 21:29:12 +0200

    Refactor rate limiting in routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const { rateLimit } = require('express-rate-limit')
+
 //import routs
 const categoryRoute = require('./categoryRoutes');
 const productRoute = require('./productRoutes');
@@ -14,14 +14,11 @@ const orderRoute = require('./orderRoutes');
 const userRoute = require('./userRoutes');
 const authRoute = require('./authRoutes');
 
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    message:"Rate limit exceeded, please try again later after 15 minutes",
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
 
+const createRateLimiter = require('../utils/appRateLimiter');
+
+// Create rate limiters for all routes that need them
+const limiter = createRateLimiter(300, "Too many requests please try again later after 15 minutes",15);
 
 
 
@@ -30,16 +27,16 @@ const mountRouts = (app) => {
 
     app.use('/api/v1/categories', categoryRoute);
     app.use('/api/v1/products',limiter, productRoute);
-    app.use('/api/v1/brands', brandRoute);
-    app.use('/api/v1/subCategories', subCategoryRoute);
+    app.use('/api/v1/brands',limiter, brandRoute);
+    app.use('/api/v1/subCategories',limiter, subCategoryRoute);
     app.use('/api/v1/users', userRoute);
     app.use('/api/v1/auth', authRoute);
-    app.use('/api/v1/reviews', reviewRoute);
-    app.use('/api/v1/wishlist', wishlistRoute);
-    app.use('/api/v1/address', addressRoute);
-    app.use('/api/v1/coupons', couponRoute);
-    app.use('/api/v1/cart', cartRoute);
-    app.use('/api/v1/orders', orderRoute);
+    app.use('/api/v1/reviews',limiter, reviewRoute);
+    app.use('/api/v1/wishlist', limiter,wishlistRoute);
+    app.use('/api/v1/address',limiter, addressRoute);
+    app.use('/api/v1/coupons',limiter, couponRoute);
+    app.use('/api/v1/cart',limiter, cartRoute);
+    app.use('/api/v1/orders',limiter, orderRoute);
 }
 
 module.exports = mountRouts;
